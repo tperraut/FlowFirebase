038aa1c2083e8c49ebf2d41f018cd58b205a35ea
t.perraut <t.perraut@groupeonepoint.com>
2019-08-19 17:05:27 +0200

    [ TaskExtension ] Revert

--- app/src/main/java/com/github/tperraut/flowfirebase/helpers/TaskExtension.kt
+++ app/src/main/java/com/github/tperraut/flowfirebase/helpers/TaskExtension.kt
@@ -1,18 +1,11 @@
 package com.github.tperraut.flowfirebase.helpers
 
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.flow
 
 @FlowPreview
 fun <TResult> Task<TResult>.asFlow() = flow {
-    addOnSuccessListener { result ->
-        suspend {
-            when (result) {
-                is Void -> emit(Unit)
-                else -> emit(result)
-            }
-        }
-    }
-    addOnFailureListener { throw it }
+    emit(Tasks.await(this@asFlow))
 }
